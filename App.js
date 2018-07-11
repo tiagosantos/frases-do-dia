3da23bd8c51a0eb99b76cfdb56c64bf01be586b7
Carlos Tiago <ontiago@gmail.com>
2018-07-11 18:58:32 -0300

    Logo + frases comunistas

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Alert } from 'react-native';
 
 type Props = {};
+
+const gerarNovaFrase = () => {
+  let numeroAleatorio = Math.random();
+  numeroAleatorio = Math.floor(numeroAleatorio * 5);
+  let frases = Array();
+  frases[0] = 'Não é a consciência do homem que lhe determina o ser, mas, ao contrário, o seu ser social que lhe determina a consciência. - Karl Max';
+  frases[1] = 'O caminho do inferno está pavimentado de boas intenções. - Karl Max';
+  frases[2] = 'Quanto menos comes, bebes, compras livros e vais ao teatro, pensas, amas, teorizas, cantas, sofres, praticas esporte, etc., mais economizas e mais cresce o teu capital. És menos, mas tens mais. Assim todas as paixões e actividades são tragadas pela cobiça. - Karl Max';
+  frases[3] = 'Os filósofos limitaram-se a interpretar o mundo de diversas maneiras; o que importa é modificá-lo. - Karl Max';
+  frases[4] = 'A religião é o suspiro da criança acabrunhada, o coração de um mundo sem coração, assim como também o espírito de uma época sem espírito. Ela é o ópio do povo. - Karl Max';
+
+  let fraseEscolhida = frases[ numeroAleatorio ];
+
+
+  Alert.alert(fraseEscolhida);
+};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Image
+          source={require('./img/lenin.jpg')}
+           style={styles.logo}
+        />
+        <TouchableOpacity
+          onPress={gerarNovaFrase}
+          style={styles.btn}
+        >
+          <Text style={styles.btnText}>Nova frase</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -34,16 +43,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fff',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  logo: {
+    width: 300,
+    height: 300
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  btn: {
+    width: 200,
+    padding: 15,
+    alignItems: 'center',
+    backgroundColor: '#FC0002'
   },
+  btnText: {
+    fontSize: 16,
+    color: '#FFFF08',
+    fontWeight: 'bold'
+  }
 });
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# frases-do-dia
+# App frase do dia Comunista
+
+## Screenshots
+
+
+<img src="https://raw.githubusercontent.com/tiagosantos/frases-do-dia-comunista/master/img/screenshot-01.png"/>
+
+
+<img src="https://raw.githubusercontent.com/tiagosantos/frases-do-dia-comunista/master/img/screenshot-02.png"/>
